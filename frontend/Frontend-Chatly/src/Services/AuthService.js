da40e56a1363b86f459b6ca70079a56a0fca65c1
JeethanTauro <tauroshanjeeth@gmail.com>
2025-09-05 22:48:39 +0530

    added dockerfile and changed URL

--- frontend/Frontend-Chatly/src/Services/AuthService.js
+++ frontend/Frontend-Chatly/src/Services/AuthService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import Signup from '../Pages/Signup';
-const URL = "http://localhost:8080";
+const URL = "http://backend:8080";
 
 const api = axios.create({
     baseURL : URL,
